da4a01d0ea4a8b7321024fa972da88203c9ce6d2
nvdmedia <118656973+nvdmedia@users.noreply.github.com>
2024-05-15 20:43:34 +0700

    haha

--- js/progress.js
+++ js/progress.js
@@ -5,14 +5,12 @@ export const progress = (() => {
     const info = document.getElementById('progress-info');
     const bar = document.getElementById('progress-bar');
 
-    const total = assets.length;
     let loaded = 0;
 
     const progress = () => {
         loaded += 1;
-
         bar.style.width = Math.min((loaded / total) * 100, 100).toString() + "%";
-        info.innerText = `Thư đang dến đợi tí nhé 💕 (${loaded}/${total}) [${parseInt((loaded / total) * 100).toFixed(0)}%]`;
+        info.innerText = `Thư đang đến 💕 (${loaded}/${total}) [${parseInt((loaded / total) * 100).toFixed(0)}%]`;
 
         if (loaded == total) {
             util.show();
